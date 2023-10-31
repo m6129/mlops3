80eff5a6a6686ccb0be7357bbf1a2e08113dc3c5
airflow <5991anton@gmail.com>
2023-10-31 17:10:35 +0000

    add mlflow

---
+++ mlartifacts/316603247229609192/b09b32781d05476e8765daf842b912e3/artifacts/test_model code/test_model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.metrics import f1_score
+import pickle
+import mlflow
+from mlflow.tracking import MlflowClient
+
+mlflow.set_tracking_uri("http://0.0.0.0:5000")
+mlflow.set_experiment("test_model")
+
+with mlflow.start_run():
+    X_test = pd.read_csv('/home/anton/mlops3/datasets/X_test.csv')
+    y_test = pd.read_csv('/home/anton/mlops3/datasets/y_test.csv')
+    with open('/home/anton/mlops3/models/ada1.pickle', 'rb') as model_file:
+        clf = pickle.load(model_file)
+        
+        predicted_label_y = clf.predict(X_test)
+        score = f1_score(predicted_label_y, y_test, average=None)
+        print("score=",score)
+        mlflow.log_artifact(local_path='/home/anton/mlops3/scripts/test_model.py',artifact_path="test_model code")
+        mlflow.log_metric("score", score)
+        mlflow.end_run()
